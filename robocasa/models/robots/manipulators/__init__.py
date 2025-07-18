0bb50c8535b99424d470439bcc1c718088e6b8cf
hogun-pikachu <hogun.kee@rllab.snu.ac.kr>
2025-07-18 12:07:13 +0900

    init

---
+++ robocasa/models/robots/manipulators/__init__.py
@@ -0,0 +1,14 @@
+from .gr1_robot import (
+    GR1FixedLowerBodyInspireHands,
+    GR1FixedLowerBodyFourierHands,
+    GR1ArmsOnlyInspireHands,
+    GR1ArmsOnlyFourierHands,
+    GR1ArmsAndWaistFourierHands,
+)
+from .g1_robot import (
+    G1FixedLowerBodyInspireHands,
+    G1FixedLowerBodyFourierHands,
+    G1ArmsOnlyInspireHands,
+    G1ArmsOnlyFourierHands,
+    G1ArmsAndWaistFourierHands,
+)
---
+++ robocasa/models/robots/manipulators/g1_robot.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+from robosuite.robots import register_robot_class
+from robosuite.models.robots import G1
+from robosuite.models.robots.manipulators.g1_robot import (
+    G1FixedLowerBody,
+    G1ArmsOnly,
+)
+
+
+@register_robot_class("LeggedRobot")
+class G1FixedLowerBodyInspireHands(G1FixedLowerBody):
+    @property
+    def default_gripper(self):
+        return {"right": "InspireRightHand", "left": "InspireLeftHand"}
+
+
+@register_robot_class("LeggedRobot")
+class G1FixedLowerBodyFourierHands(G1FixedLowerBody):
+    @property
+    def default_gripper(self):
+        return {"right": "FourierRightHand", "left": "FourierLeftHand"}
+
+
+@register_robot_class("LeggedRobot")
+class G1ArmsOnlyInspireHands(G1ArmsOnly):
+    @property
+    def default_gripper(self):
+        return {"right": "InspireRightHand", "left": "InspireLeftHand"}
+
+
+@register_robot_class("LeggedRobot")
+class G1ArmsOnlyFourierHands(G1ArmsOnly):
+    @property
+    def default_gripper(self):
+        return {"right": "FourierRightHand", "left": "FourierLeftHand"}
+
+
+@register_robot_class("LeggedRobot")
+class G1ArmsAndWaist(G1):
+    def __init__(self, idn=0):
+        super().__init__(idn=idn)
+        self._remove_joint_actuation("leg")
+        self._remove_joint_actuation("head")
+        self._remove_free_joint()
+
+    @property
+    def init_qpos(self):
+        init_qpos = np.array([0.0] * 17)
+        right_arm_init = np.array([0.0, -0.1, 0.0, -1.57, 0.0, 0.0, 0.0])
+        left_arm_init = np.array([0.0, 0.1, 0.0, -1.57, 0.0, 0.0, 0.0])
+        init_qpos[3:10] = right_arm_init
+        init_qpos[10:17] = left_arm_init
+        return init_qpos
+
+
+@register_robot_class("LeggedRobot")
+class G1ArmsAndWaistFourierHands(G1ArmsAndWaist):
+    pass
